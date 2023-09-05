631c7b5dafd24c1f032f5726e4b5f80940d89e73
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-05 17:52:59 +0200

    add remainingHours

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,20 @@ Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
 */
 
-// data ora minuti e secondi di quando ci sarà la correzione
+// data ora minuti e secondi di quando ci sarà la correzione in millisecondi
 let countDownDate = new Date("Sep 6, 2023 09:30:00").getTime();
 console.log(countDownDate);
 
-// data corrente al momento dell caricamento della pagina
+// data corrente al momento del caricamento della pagina in millisecondi
 let currentDate = new Date().getTime();
 console.log(currentDate);
 
+// tempo rimasto alla lezione in millisecondi
 let remainingTime = countDownDate - currentDate;
 console.log(remainingTime);
+
+// ore rimaste alla lezione
+let remainingHours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+console.log(hours);
+
+// minuti rimasti alla lezione
