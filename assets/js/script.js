80cecd74a87b000607bcd882a516ad740063ebb5
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-05 15:30:34 +0200

    add countDownDate

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,7 @@ Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
 */
 
+// indicare data ora minuti e secondi di quando ci sarà la correzione
+
+let countDownDate = new Date("Sep 6, 2023 09:30:00").getTime();
+console.log(countDownDate);
