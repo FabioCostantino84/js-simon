55505ca87081d4df523d6dd99871440fffad22f5
FabioCostantino84 <costantinofabio84@gmail.com>
2023-09-05 15:36:46 +0200

    add currentDate

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,10 @@ Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione
 Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
 */
 
-// indicare data ora minuti e secondi di quando ci sarà la correzione
-
+// data ora minuti e secondi di quando ci sarà la correzione
 let countDownDate = new Date("Sep 6, 2023 09:30:00").getTime();
 console.log(countDownDate);
+
+// data corrente al momento dell caricamento della pagina
+let currentDate = new Date().getTime();
+console.log(currentDate);
